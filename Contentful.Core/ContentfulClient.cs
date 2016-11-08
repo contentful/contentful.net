e67e3a44893d780049027874b03c9b541a79dfa5
Robert Linde <robert@selberglinde.se>
2016-11-08 19:26:36 +0100

    Add solution, project and client classes

---
+++ Contentful.Core/ContentfulClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contentful.Core
+{
+    /// <summary>
+    /// Main class for interaction with the contentful delivery and preview APIs
+    /// </summary>
+    public class ContentfulClient : IContentfulClient
+    {
+    }
+}
---
+++ Contentful.Core/IContentfulClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contentful.Core
+{
+    /// <summary>
+    /// Defines a set of methods to call underlying api endpoints at contentful.
+    /// </summary>
+    public interface IContentfulClient
+    {
+    }
+}
