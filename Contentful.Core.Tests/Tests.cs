f63297b8b792bde3bed538f4ee9f6c62d042c156
Robert Linde <robert@selberglinde.se>
2016-11-09 09:37:10 +0100

    Add unit test project

---
+++ Contentful.Core.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Contentful.Core;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
