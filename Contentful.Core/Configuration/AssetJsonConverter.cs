9544cc3f31c4a97c4a3eaf0bd32def1cdf9dd94a
Robert Linde <robert@selberglinde.se>
2016-11-17 12:31:38 +0100

    Add comments for converters

--- Contentful.Core/Configuration/AssetJsonConverter.cs
+++ Contentful.Core/Configuration/AssetJsonConverter.cs
@@ -8,11 +8,25 @@ using System.Threading.Tasks;
 
 namespace Contentful.Core.Configuration
 {
+    /// <summary>
+    /// JsonConverter for converting assets into a simpler structure.
+    /// </summary>
     public class AssetJsonConverter : JsonConverter
     {
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">The type to convert to.</param>
         public override bool CanConvert(Type objectType) => objectType == typeof(Asset);
 
-
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The reader to use.</param>
+        /// <param name="objectType">The object type to serialize into.</param>
+        /// <param name="existingValue">The current value of the property.</param>
+        /// <param name="serializer">The serializer to use.</param>
+        /// <returns>The deserialized <see cref="Asset"/>.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.Null) {
@@ -31,6 +45,13 @@ namespace Contentful.Core.Configuration
             return asset;
         }
 
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// **NOTE: This method is not implemented and will throw an exception.**
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="serializer"></param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new NotImplementedException();
--- Contentful.Core/Configuration/EntryFieldJsonConverter.cs
+++ Contentful.Core/Configuration/EntryFieldJsonConverter.cs
@@ -8,11 +8,21 @@ using Contentful.Core.Models;
 
 namespace Contentful.Core.Configuration
 {
+    /// <summary>
+    /// JsonConverter for converting entry fields into a cutomst object.
+    /// </summary>
     public class EntryFieldJsonConverter : JsonConverter
     {
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">The type to convert to.</param>
         public override bool CanConvert(Type objectType) => true;
         private bool _canRead = true;
 
+        /// <summary>
+        /// Gets a value indicating whether this converter can currently read JSON.
+        /// </summary>
         public override bool CanRead
         {
             get
@@ -21,6 +31,14 @@ namespace Contentful.Core.Configuration
             }
         }
 
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The reader to use.</param>
+        /// <param name="objectType">The object type to serialize into.</param>
+        /// <param name="existingValue">The current value of the property.</param>
+        /// <param name="serializer">The serializer to use.</param>
+        /// <returns>The deserialized object.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.Null)
@@ -38,6 +56,13 @@ namespace Contentful.Core.Configuration
             return returnObject;
         }
 
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// **NOTE: This method is not implemented and will throw an exception.**
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="serializer"></param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new NotImplementedException();
