b77078b83512d2313247fbe1b09e0ddd1f622a5a
Robert Linde <robert@selberglinde.se>
2016-11-18 16:12:05 +0100

    Add all operations for spaces

--- Contentful.Core/ContentfulManagementClient.cs
+++ Contentful.Core/ContentfulManagementClient.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -64,17 +65,15 @@ namespace Contentful.Core
         /// Initializes a new instance of the <see cref="ContentfulManagementClient"/> class.
         /// </summary>
         /// <param name="httpClient">The HttpClient of your application.</param>
-        /// <param name="deliveryApiKey">The delivery API key used when communicating with the Contentful API</param>
+        /// <param name="managementApiKey">The management API key used when communicating with the Contentful API</param>
         /// <param name="spaceId">The ID of the space to fetch content from.</param>
-        /// <param name="usePreviewApi">Whether or not to use the Preview API for requests.
         /// If this is set to true the preview API key needs to be used for <paramref name="deliveryApiKey"/>
         ///  </param>
-        public ContentfulManagementClient(HttpClient httpClient, string deliveryApiKey, string spaceId, bool usePreviewApi = false):
+        public ContentfulManagementClient(HttpClient httpClient, string deliveryApiKey, string spaceId):
             this(httpClient, new OptionsWrapper<ContentfulOptions>(new ContentfulOptions()
             {
                 DeliveryApiKey = deliveryApiKey,
-                SpaceId = spaceId,
-                UsePreviewApi = usePreviewApi
+                SpaceId = spaceId
             }))
         {
 
@@ -86,8 +85,8 @@ namespace Contentful.Core
         /// <param name="name">The name of the space to create.</param>
         /// <param name="defaultLocale">The default locale for this space.</param>
         /// <param name="organisation">The organisation to create a space for. Not required if the account belongs to only one organisation.</param>
-        /// <returns></returns>
-        public async Task<Space> CreateSpace(string name, string defaultLocale, string organisation = null)
+        /// <returns>The created <see cref="Space"/></returns>
+        public async Task<Space> CreateSpaceAsync(string name, string defaultLocale, string organisation = null)
         {
             if (!string.IsNullOrEmpty(organisation))
             {
@@ -106,6 +105,106 @@ namespace Contentful.Core
             var json = JObject.Parse(await res.Content.ReadAsStringAsync());
 
             return json.ToObject<Space>();
+        }
+
+        /// <summary>
+        /// Updates the name of a space in Contentful.
+        /// </summary>
+        /// <param name="space">The space to update, needs to contain at minimum name, Id and version.</param>
+        /// <param name="organisation">The organisation to update a space for. Not required if the account belongs to only one organisation.</param>
+        /// <returns>The updated <see cref="Space"/></returns>
+        public async Task<Space> UpdateSpaceNameAsync(Space space, string organisation = null)
+        {
+            return await UpdateSpaceNameAsync(space.SystemProperties.Id, space.Name, space.SystemProperties.Version ?? 1, organisation);
+        }
+
+        /// <summary>
+        /// Updates a space in Contentful.
+        /// </summary>
+        /// <param name="id">The id of the space to update.</param>
+        /// <param name="name">The name to update to.</param>
+        /// <param name="version">The version of the space that will be updated.</param>
+        /// <param name="organisation">The organisation to update a space for. Not required if the account belongs to only one organisation.</param>
+        /// <returns>The updated <see cref="Space"/></returns>
+        public async Task<Space> UpdateSpaceNameAsync(string id, string name, int version, string organisation = null)
+        {
+            if (!string.IsNullOrEmpty(organisation))
+            {
+                _httpClient.DefaultRequestHeaders.Add("X-Contentful-Organization", organisation);
+            }
+
+            if (_httpClient.DefaultRequestHeaders.Contains("X-Contentful-Version"))
+            {
+                _httpClient.DefaultRequestHeaders.Remove("X-Contentful-Version");
+            }
+
+            _httpClient.DefaultRequestHeaders.Add("X-Contentful-Version", version.ToString());
+
+            var res = await _httpClient.PutAsync($"{_baseUrl}{id}", ConvertObjectToJsonStringContent(new { name = name }));
+
+            _httpClient.DefaultRequestHeaders.Remove("X-Contentful-Organization");
+            _httpClient.DefaultRequestHeaders.Remove("X-Contentful-Version");
+
+            if (!res.IsSuccessStatusCode)
+            {
+                await CreateExceptionForFailedRequestAsync(res);
+            }
+
+            var json = JObject.Parse(await res.Content.ReadAsStringAsync());
+
+            return json.ToObject<Space>();
+        }
+
+        /// <summary>
+        /// Gets a space in Contentful.
+        /// </summary>
+        /// <param name="id">The id of the space to get.</param>
+        /// <returns>The <see cref="Space" /></returns>
+        public async Task<Space> GetSpaceAsync(string id)
+        {
+            var res = await _httpClient.GetAsync($"{_baseUrl}{id}");
+
+            if (!res.IsSuccessStatusCode)
+            {
+                await CreateExceptionForFailedRequestAsync(res);
+            }
+
+            var json = JObject.Parse(await res.Content.ReadAsStringAsync());
+
+            return json.ToObject<Space>();
+        }
+
+        /// <summary>
+        /// Gets all spaces in Contentful.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Space>> GetSpacesAsync()
+        {
+            var res = await _httpClient.GetAsync(_baseUrl);
+
+            if (!res.IsSuccessStatusCode)
+            {
+                await CreateExceptionForFailedRequestAsync(res);
+            }
+
+            var json = JObject.Parse(await res.Content.ReadAsStringAsync());
+
+            return json.SelectTokens("$..items[*]").Select(t => t.ToObject<Space>()); ;
+        }
+
+        /// <summary>
+        /// Deletes a space in Contentful.
+        /// </summary>
+        /// <param name="id">The id of the space to delete.</param>
+        /// <returns></returns>
+        public async Task DeleteSpaceAsync(string id)
+        {
+            var res = await _httpClient.DeleteAsync($"{_baseUrl}{id}");
+
+            if (!res.IsSuccessStatusCode)
+            {
+                await CreateExceptionForFailedRequestAsync(res);
+            }
         }
 
         private StringContent ConvertObjectToJsonStringContent(object ob)
