22825e09f909bae10d9938ba26bcd63b4579e461
Robert Linde <robert@selberglinde.se>
2016-12-05 23:04:29 +0100

    Add service collection extension and taghelper

---
+++ Contentful.AspNetCore/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Contentful.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using Contentful.Core;
+
+namespace Contentful.AspNetCore
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContentful(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.AddOptions();
+            services.Configure<ContentfulOptions>(configuration.GetSection("ContentfulOptions"));
+            services.TryAddSingleton<HttpClient>();
+            services.TryAddTransient<IContentfulClient, ContentfulClient>();
+            services.TryAddTransient<IContentfulManagementClient, ContentfulManagementClient>();
+            return services;
+        }
+    }
+}
