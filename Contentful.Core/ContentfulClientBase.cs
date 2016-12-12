12a321970f2be8c132236dba8feb34c42cf3329f
Robert Linde <robert@selberglinde.se>
2016-12-12 10:28:46 +0100

    Add better error messages for generic http errors

--- Contentful.Core/ContentfulClientBase.cs
+++ Contentful.Core/ContentfulClientBase.cs
@@ -21,13 +21,82 @@ namespace Contentful.Core
             var jsonError = JObject.Parse(await res.Content.ReadAsStringAsync().ConfigureAwait(false));
             var sys = jsonError.SelectToken("$.sys").ToObject<SystemProperties>();
             var errorDetails = jsonError.SelectToken("$.details")?.ToObject<ErrorDetails>();
-            var ex = new ContentfulException((int)res.StatusCode, jsonError.SelectToken("$.message")?.ToString() ?? "Something went wrong")
+
+            var message = jsonError.SelectToken("$.message")?.ToString();
+            var statusCode = (int)res.StatusCode;
+
+            if (string.IsNullOrEmpty(message))
+            {
+                message = GetGenericErrorMessageForStatusCode(statusCode, sys.Id);
+            }
+
+            var ex = new ContentfulException(statusCode, message)
             {
                 RequestId = jsonError.SelectToken("$.requestId")?.ToString(),
                 ErrorDetails = errorDetails,
                 SystemProperties = sys
             };
             throw ex;
+        }
+
+        private string GetGenericErrorMessageForStatusCode(int statusCode, string id)
+        {
+            if(statusCode == 400)
+            {
+                if (id == "BadRequestError")
+                {
+                    return "The request was malformed or missing a required parameter.";
+                }
+
+                return "The request contained invalid or unknown query parameters.";
+            }
+
+            if(statusCode == 401)
+            {
+                return "The authorization token was invalid.";
+            }
+
+            if(statusCode == 403)
+            {
+                return "The specified token does not have access to the requested resource.";
+            }
+
+            if(statusCode == 404)
+            {
+                return "The requested resource or endpoint could not be found.";
+            }
+
+            if (statusCode == 409)
+            {
+                return "Version mismatch error. The version you specified was incorrect. This may be due to someone else editing the content.";
+            }
+
+            if (statusCode == 422)
+            {
+                if(id == "InvalidEntryError")
+                {
+                    return "The entered value was invalid.";
+                }
+
+                return "Validation failed. The request references an invalid field.";
+            }
+
+            if(statusCode == 429)
+            {
+                return "Rate limit exceeded. Too many requests per second.";
+            }
+
+            if(statusCode == 500)
+            {
+                return "Internal server error.";
+            }
+
+            if(statusCode == 502)
+            {
+                return "The requested space is hibernated.";
+            }
+
+            return "An error occurred.";
         }
 
         protected async Task<HttpResponseMessage> SendHttpRequestAsync(string url, HttpMethod method, string authToken, HttpContent content = null)
