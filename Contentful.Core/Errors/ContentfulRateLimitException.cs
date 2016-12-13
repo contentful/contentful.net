a19a8a2c701df7faeb48401486ffcf306ab480fd
Robert Linde <robert@selberglinde.se>
2016-12-13 19:56:51 +0100

    Add comments

--- Contentful.Core/Errors/ContentfulRateLimitException.cs
+++ Contentful.Core/Errors/ContentfulRateLimitException.cs
@@ -4,12 +4,22 @@ using System.Text;
 
 namespace Contentful.Core.Errors
 {
+    /// <summary>
+    /// Represents errors that occurr when a call hit the rate limit of the API.
+    /// </summary>
     public class ContentfulRateLimitException : ContentfulException
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ContentulRateLimitException"/>.
+        /// </summary>
+        /// <param name="message">The message of the exception.</param>
         public ContentfulRateLimitException(string message) : base(429, message)
         {
         }
 
+        /// <summary>
+        /// The number of seconds until the next request can be made to the API.
+        /// </summary>
         public int SecondsUntilNextRequest { get; set; }
     }
 }
