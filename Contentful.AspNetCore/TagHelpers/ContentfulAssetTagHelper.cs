79f1a3a9407f3652df477f92c429634e298b7e1b
Robert Linde <robert@selberglinde.se>
2016-12-20 09:49:32 +0100

    Add simple asset tag helper

---
+++ Contentful.AspNetCore/TagHelpers/ContentfulAssetTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Contentful.Core;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contentful.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "asset-id")]
+    public class ContentfulAssetTagHelper : TagHelper
+    {
+        private readonly IContentfulClient _client;
+
+        public ContentfulAssetTagHelper(IContentfulClient client)
+        {
+            _client = client;
+        }
+
+        public string AssetId { get; set; }
+
+        public string Locale { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(AssetId))
+            {
+                return;
+            }
+
+            var asset = await _client.GetAssetAsync(AssetId, Locale);
+
+            output.Attributes.RemoveAll("asset-id");
+            output.Attributes.RemoveAll("locale");
+
+            output.Attributes.SetAttribute("href", asset.File.Url);
+        }
+    }
+}
