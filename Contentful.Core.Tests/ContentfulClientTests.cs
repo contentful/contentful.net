0e2bce5cdebfc6c022f62f2ff690036a376ba091
Robert Linde <robert@selberglinde.se>
2017-01-20 15:32:58 +0100

    Update test to include another locale

--- Contentful.Core.Tests/ContentfulClientTests.cs
+++ Contentful.Core.Tests/ContentfulClientTests.cs
@@ -356,6 +356,7 @@ namespace Contentful.Core.Tests
             //Assert
             Assert.Equal(2, res.Count());
             Assert.Equal("Alice in Wonderland", res.IncludedAssets.Single(c => c.SystemProperties.Id == "AssetId4").TitleLocalized["en-US"]);
+            Assert.Equal("Alice i underlandet", res.IncludedAssets.Single(c => c.SystemProperties.Id == "AssetId4").TitleLocalized["sv-SE"]);
             Assert.Equal("Lewis Carroll", res.IncludedEntries.Single(c => c.SystemProperties.Id == "EntryId66").Fields.name["en-US"].ToString());
         }
 
