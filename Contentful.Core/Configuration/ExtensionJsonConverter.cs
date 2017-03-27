07a06de3d2c61a3baac25403885827ac3c35ec6a
Robert Linde <robert@selberglinde.se>
2017-03-27 12:32:54 +0200

    Add extension endpoints methods

---
+++ Contentful.Core/Configuration/ExtensionJsonConverter.cs
@@ -0,0 +1,83 @@
+﻿using Contentful.Core.Models;
+using Contentful.Core.Models.Management;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contentful.Core.Configuration
+{
+    /// <summary>
+    /// JsonConverter for converting <see cref="Contentful.Core.Models.Management.UiExtension"/>.
+    /// </summary>
+    public class ExtensionJsonConverter : JsonConverter
+    {
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">The type to convert to.</param>
+        public override bool CanConvert(Type objectType) => objectType == typeof(UiExtension);
+
+        /// <summary>
+        /// Gets a value indicating whether this JsonConverter can write JSON.
+        /// </summary>
+        public override bool CanWrite => true;
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The reader to use.</param>
+        /// <param name="objectType">The object type to serialize into.</param>
+        /// <param name="existingValue">The current value of the property.</param>
+        /// <param name="serializer">The serializer to use.</param>
+        /// <returns>The deserialized <see cref="Asset"/>.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
+            var jObject = JObject.Load(reader);
+            var extensionProperties = jObject.SelectToken("$.extension");
+
+            var extension = new UiExtension();
+
+            extension.SystemProperties = jObject["sys"].ToObject<SystemProperties>();
+            extension.Src = extensionProperties["src"]?.ToString();
+            extension.Name = extensionProperties["name"]?.ToString();
+            extension.Sidebar = extensionProperties["sidebar"]?.Value<bool>() ?? false;
+            extension.FieldTypes = extensionProperties["fieldTypes"]?.Value<List<string>>();
+            extension.SrcDoc = extensionProperties["srcDoc"]?.ToString();
+
+            return extension;
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="serializer"></param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var extension = value as UiExtension;
+
+            if (extension == null)
+            {
+                return;
+            }
+
+            serializer.Serialize(writer, 
+                new {
+                    sys = extension.SystemProperties,
+                    extension = new {
+                        src = extension.Src,
+                        name = extension.Name,
+                        fieldTypes = extension.FieldTypes,
+                        srcDoc = extension.SrcDoc,
+                        sidebar = extension.Sidebar } });
+        }
+    }
+}
---
+++ Contentful.Core/Models/Management/UiExtension.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contentful.Core.Models.Management
+{
+    /// <summary>
+    /// Encapsulates information about a Contentful Ui Extension
+    /// </summary>
+    public class UiExtension : IContentfulResource
+    {
+        /// <summary>
+        /// Common system managed metadata properties.
+        /// </summary>
+        [JsonProperty("sys")]
+        public SystemProperties SystemProperties { get; set; }
+
+        /// <summary>
+        /// The source URL for html file for the extension.
+        /// </summary>
+        public string Src { get; set; }
+
+        /// <summary>
+        /// String representation of the widget, e.g. inline HTML.
+        /// </summary>
+        public string SrcDoc { get; set; }
+
+        /// <summary>
+        /// The name of the extension
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The field types for which this extension applies.
+        /// </summary>
+        public List<string> FieldTypes { get; set; }
+
+        /// <summary>
+        /// Whether or not this is an extension for the Contentful sidebar.
+        /// </summary>
+        public bool Sidebar { get; set; }
+    }
+}
