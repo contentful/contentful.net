73192601058fd175ad6da8eab3f24e3cf11c9ffb
Robert Linde <robert@selberglinde.se>
2017-05-01 21:46:34 +0200

    Added ContentTypeResolver

---
+++ Contentful.Core/Configuration/IContentTypeResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contentful.Core.Configuration
+{
+    public interface IContentTypeResolver
+    {
+        Type Resolve(string contentTypeId);
+    }
+}
--- Contentful.Core/ContentfulClient.cs
+++ Contentful.Core/ContentfulClient.cs
@@ -84,6 +84,11 @@ namespace Contentful.Core
         public bool IsPreviewClient => _options?.UsePreviewApi ?? false;
 
         /// <summary>
+        /// Gets or sets the resolver used when resolving entries to typed objects.
+        /// </summary>
+        public IContentTypeResolver ContentTypeResolver { get; set; }
+
+        /// <summary>
         /// Get a single entry by the specified ID.
         /// </summary>
         /// <typeparam name="T">The type to serialize this entry into. If you want the metadata to 
