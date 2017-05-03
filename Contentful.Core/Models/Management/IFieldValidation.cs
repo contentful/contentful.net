6cb1d9e933ed779aa1a49ef028bd572977ba2471
mercedesb <mercedesbernard@live.com>
2017-05-02 22:47:03 -0500

    added file size validator

--- Contentful.Core/Models/Management/IFieldValidation.cs
+++ Contentful.Core/Models/Management/IFieldValidation.cs
@@ -302,6 +302,7 @@ namespace Contentful.Core.Models.Management
             return new { unique = true };
         }
     }
+
     /// <summary>
     /// Represents a validator that ensures that the field value is within a certain date range.
     /// </summary>  
@@ -362,6 +363,59 @@ namespace Contentful.Core.Models.Management
 		public object CreateValidator()
 		{
 			return new { dateRange = new { min = _min, max = _max }, message = this.Message };
+		}
+	}
+
+    /// <summary>
+    /// Represents a validator that ensures that the media file size is within a certain size range.
+    /// </summary>      
+    public class FileSizeValidator : IFieldValidator
+	{
+		protected const int BYTES_IN_KB = 1024;
+		protected const int BYTES_IN_MB = 1048576;
+		/// <summary>
+		/// The minimum allowed size of the file.
+		/// </summary>
+		public int? Min { get; set; }
+
+		/// <summary>
+		/// The maximum allowed size of the file.
+		/// </summary>
+		public int? Max { get; set; }
+		/// <summary>
+		/// The custom error message that should be displayed.
+		/// </summary>
+		public string Message { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="T:Contentful.Essential.Models.Management.FileSizeValidator" />.
+		/// </summary>
+		/// <param name="min">The minimum size of the file.</param>
+		/// <param name="max">The maximum size of the file.</param>
+		/// <param name="minUnit">The unit measuring the minimum file size.</param>
+		/// <param name="maxUnit">The unit measuring the maximum file size.</param>
+		/// <param name="message">The custom error message for this validation.</param>
+		public FileSizeValidator(int? min, int? max, string minUnit = SystemFileSizeUnits.Bytes, string maxUnit = SystemFileSizeUnits.Bytes, string message = null)
+		{
+			this.Min = GetCalculatedByteSize(min, minUnit);
+			this.Max = GetCalculatedByteSize(max, maxUnit);
+			this.Message = message;
+		}
+		public object CreateValidator()
+		{
+			return new { assetFileSize = new { min = this.Min, max = this.Max }, message = this.Message };
+		}
+
+		protected virtual int? GetCalculatedByteSize(int? value, string unit)
+		{
+			if (value != null)
+			{
+				if (unit == SystemFileSizeUnits.KB)
+					value = value * 1000;
+				if (unit == SystemFileSizeUnits.MB)
+					value = value * 1048576;
+			}
+			return value;
 		}
 	}
 }
