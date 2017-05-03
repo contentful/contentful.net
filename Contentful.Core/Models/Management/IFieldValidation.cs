02f17d117ff006a1dbe9046f7dc204ecb100b98d
mercedesb <mercedesbernard@live.com>
2017-05-02 22:51:23 -0500

    added image size validator

--- Contentful.Core/Models/Management/IFieldValidation.cs
+++ Contentful.Core/Models/Management/IFieldValidation.cs
@@ -418,4 +418,57 @@ namespace Contentful.Core.Models.Management
 			return value;
 		}
 	}
+    
+    /// <summary>
+    /// Represents a validator that ensures that the image dimensions are within a certain range.
+    /// </summary>   
+    public class ImageSizeValidator : IFieldValidator
+	{
+		/// <summary>
+		/// The minimum allowed width of the image (in px).
+		/// </summary>
+		public int? MinWidth { get; set; }
+
+		/// <summary>
+		/// The maximum allowed width of the image (in px).
+		/// </summary>
+		public int? MaxWidth { get; set; }
+		/// <summary>
+		/// The minimum allowed height of the iamge (in px).
+		/// </summary>
+		int? MinHeight { get; set; }
+
+		/// <summary>
+		/// The maximum allowed height of the image (in px).
+		/// </summary>
+		public int? MaxHeight { get; set; }
+
+		/// <summary>
+		/// The custom error message that should be displayed.
+		/// </summary>
+		public string Message { get; set; }
+
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="ImageSizeValidator" />.
+		/// </summary>
+		/// <param name="minWidth">The minimum width of the image.</param>
+		/// <param name="maxWidth">The maximum width of the image.</param>
+		/// <param name="minHeight">The minimum height of the image.</param>
+		/// <param name="maxHeight">The maximum height of the image.</param>
+		/// <param name="message">The custom error message for this validation.</param>
+		public ImageSizeValidator(int? minWidth, int? maxWidth, int? minHeight, int? maxHeight, string message = null)
+		{
+			this.MinWidth = minWidth;
+			this.MaxWidth = maxWidth;
+			this.MinHeight = minHeight;
+			this.MaxHeight = maxHeight;
+			this.Message = message;
+		}
+
+		public object CreateValidator()
+		{
+			return new { assetImageDimensions = new { width = new { min = this.MinWidth, max = this.MaxWidth }, height = new { min = this.MinHeight, max = this.MaxHeight } }, message = this.Message };
+		}
+	}
 }
