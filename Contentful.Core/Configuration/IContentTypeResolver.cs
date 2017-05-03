fc40443f871c0b25cc460f620f94a2bf109e776d
Robert Linde <robert@selberglinde.se>
2017-05-03 13:02:10 +0200

    Increase package version and dependency

--- Contentful.Core/Configuration/IContentTypeResolver.cs
+++ Contentful.Core/Configuration/IContentTypeResolver.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace Contentful.Core.Configuration
 {
+    /// <summary>
+    /// An interface for a contenttype resolver to allow resolving contenttypes to concrete CLR types.
+    /// </summary>
     public interface IContentTypeResolver
     {
+        /// <summary>
+        /// Resolves a content type id to a concrete type.
+        /// </summary>
+        /// <param name="contentTypeId">The contenttype id to resolve.</param>
+        /// <returns>The type for the provided contenttype id.</returns>
         Type Resolve(string contentTypeId);
     }
 }
