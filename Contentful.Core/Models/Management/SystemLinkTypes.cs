71ce2917adf94bce4c87702edb658891efe65bf0
mercedesb <mercedesbernard@live.com>
2017-05-09 13:25:27 -0500

    added SystemLinkTypes consts

---
+++ Contentful.Core/Models/Management/SystemLinkTypes.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contentful.Core.Models.Management
+{
+    /// <summary>
+    /// Represents the different types available for the LinkType of a <see cref="Field"/>.
+    /// </summary>
+    public class SystemLinkTypes
+    {
+        /// <summary>
+        /// A reference to an entry.
+        /// </summary>
+        public const string Entry = "Entry";
+        /// <summary>
+        /// A reference to an asset.
+        /// </summary>
+        public const string Asset = "Asset";
+    }
+}
