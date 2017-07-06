71cabe262fa3b121d68dbf06c7dea4ceb7ceebb9
Robert Linde <robert@selberglinde.se>
2017-07-06 08:01:01 +0200

    Add check for type missing in serialization

--- Contentful.Core/ContentfulClient.cs
+++ Contentful.Core/ContentfulClient.cs
@@ -227,7 +227,7 @@ namespace Contentful.Core
                     var token = entryTokens[i];
                     var grandParent = token.Parent.Parent;
 
-                    if(grandParent["sys"] != null && grandParent["sys"]["type"]?.ToString() != "Entry")
+                    if(grandParent["sys"]?["type"] != null && grandParent["sys"]["type"]?.ToString() != "Entry")
                     {
                         continue;
                     }
