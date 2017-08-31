d29c4f1a330c6d23de5bcd94a9a0c0a494d92875
Robert Linde <robert@selberglinde.se>
2017-08-31 12:49:37 +0200

    Add more tests for create entry with locale

--- Contentful.Core.Tests/ContentfulManagementClientTests.cs
+++ Contentful.Core.Tests/ContentfulManagementClientTests.cs
@@ -755,6 +755,50 @@ namespace Contentful.Core.Tests
 
             entry.Field1 = "Fischer";
             _handler.Responses.Enqueue(GetResponseFromFile(@"SampleEntryManagement.json"));
+            var contentSet = "";
+
+            _handler.VerifyRequest = async (HttpRequestMessage request) =>
+            {
+                contentSet = await (request.Content as StringContent).ReadAsStringAsync();
+            };
+
+            //Act
+            var res = await _client.CreateEntryForLocaleAsync(entry, "532", "contentType", locale: "sv-SE");
+            //Assert
+
+            Assert.Contains(@"""field1"":{""sv-SE"":""Fischer""}", contentSet);
+        }
+
+        [Fact]
+        public async Task CreateEntryForLocaleShouldSetCorrectContentTypeHeader()
+        {
+            //Arrange
+            var entry = new TestNested();
+
+            entry.Field1 = "Fischer";
+            _handler.Responses.Enqueue(GetResponseFromFile(@"SampleEntryManagement.json"));
+            var contentTypeHeader = "";
+            _handler.VerificationBeforeSend = () =>
+            {
+                contentTypeHeader = _httpClient.DefaultRequestHeaders.GetValues("X-Contentful-Content-Type").First();
+            };
+  
+
+            //Act
+            var res = await _client.CreateEntryForLocaleAsync(entry, "532", "contentType", locale: "sv-SE");
+            //Assert
+
+            Assert.Contains("contentType", contentTypeHeader);
+        }
+
+        [Fact]
+        public async Task CreateEntryForLocaleShouldSetValuesCorrectlyWhenNoLocaleIsSpecified()
+        {
+            //Arrange
+            var entry = new TestNested();
+
+            entry.Field1 = "Alekhine";
+            _handler.Responses.Enqueue(GetResponseFromFile(@"LocalesCollection.json"));
             _handler.Responses.Enqueue(GetResponseFromFile(@"SampleEntryManagement.json"));
             var contentSet = "";
 
@@ -764,10 +808,10 @@ namespace Contentful.Core.Tests
             };
 
             //Act
-            var res = await _client.CreateEntryForLocaleAsync(entry, "532", "contentType", locale: "en-US");
+            var res = await _client.CreateEntryForLocaleAsync(entry, "532", "contentType");
             //Assert
 
-            Assert.Contains(@"""field1"":{""en-US"":""Fischer""}", contentSet);
+            Assert.Contains(@"""field1"":{""en-US"":""Alekhine""}", contentSet);
         }
 
         [Fact]
