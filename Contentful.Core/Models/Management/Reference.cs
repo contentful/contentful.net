cbfb916d5276959426490a5a1bfcde151adefad3
Robert Linde <robert@selberglinde.se>
2017-10-17 13:53:50 +0200

    Increase package version and dependency

--- Contentful.Core/Models/Management/Reference.cs
+++ Contentful.Core/Models/Management/Reference.cs
@@ -4,15 +4,36 @@ using System.Text;
 
 namespace Contentful.Core.Models.Management
 {
+    /// <summary>
+    /// Represents a reference link returned from the Contentful API.
+    /// Allows you to easily model reference fields when creating new entries.
+    /// </summary>
     public class Reference
     {
-        public string Sys { get; set; }
+        /// <summary>
+        /// The properties for this reference.
+        /// </summary>
+        public ReferenceProperties Sys { get; set; }
     }
 
+    /// <summary>
+    /// Encapsulates the three properties that a <see cref="Reference"/> consists of.
+    /// </summary>
     public class ReferenceProperties
     {
+        /// <summary>
+        /// The type of object, for references this is always "Link".
+        /// </summary>
         public string Type => "Link";
+
+        /// <summary>
+        /// The type of link. Normally one of <see cref="SystemLinkTypes"/>.
+        /// </summary>
         public string LinkType { get; set; }
+
+        /// <summary>
+        /// The if of the item which is being referenced.
+        /// </summary>
         public string Id { get; set; }
     }
 }
