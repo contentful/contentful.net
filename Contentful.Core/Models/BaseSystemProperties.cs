fb7ef035d79a52f8614af9219fd6f3e00368ab2a
Dennis Bidstrup <deb@impact.dk>
2024-09-12 14:11:20 +0200

    Added support for scheduled endpoints in the management api. Extended contentfulCollections to support cursor paging

---
+++ Contentful.Core/Models/BaseSystemProperties.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Contentful.Core.Models.Management;
+
+namespace Contentful.Core.Models;
+
+public class BaseSystemProperties
+{
+    /// <summary>
+    /// The unique identifier of the resource.
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// The type of link. Will be null for non link types.
+    /// </summary>
+    public string LinkType { get; set; }
+
+    /// <summary>
+    /// The type of the resource.
+    /// </summary>
+    public string Type { get; set; }
+
+    /// <summary>
+    /// The link to the environment of the resource.
+    /// </summary>
+    public ContentfulEnvironment Environment { get; set; }
+
+    /// <summary>
+    /// The link to the space of the resource.
+    /// </summary>
+    public Space Space { get; set; }
+
+    /// <summary>
+    /// The date and time the resource was created. Will be null when not applicable, e.g. for arrays.
+    /// </summary>
+    public DateTime? CreatedAt { get; set; }
+
+    /// <summary>
+    /// The link to the user that created this content. Will only be present for management API call.
+    /// </summary>
+    public User CreatedBy { get; set; }
+
+    /// <summary>
+    /// The date and time the resource was last updated. Will be null when not applicable or when the resource has never been updated.
+    /// </summary>
+    public DateTime? UpdatedAt { get; set; }
+
+    /// <summary>
+    /// The link to the user that last updated this content. Will only be present for management API call.
+    /// </summary>
+    public User UpdatedBy { get; set; }
+        
+    /// <summary>
+    /// The current version of the resource. Will only be present for management API calls. 
+    /// </summary>
+    public int? Version { get; set; }
+}
---
+++ Contentful.Core/Models/Management/ActionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Contentful.Core.Models.Management;
+
+public enum ScheduledActionStatus
+{
+    Canceled = 0,
+    Failed = 1,
+    Scheduled = 2,
+    Succeeded = 3
+}
