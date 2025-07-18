96d710b848f186c3a9a0bddf472dcd6c76759225
Bhushan Lodha <bhushanlodha@gmail.com>
2025-07-18 10:25:58 +0530

    Fix SystemProperties serialization to ignore null values

--- Contentful.Core.Tests/Extensions/JsonConversionExtensionsTests.cs
+++ Contentful.Core.Tests/Extensions/JsonConversionExtensionsTests.cs
@@ -1,4 +1,5 @@
 using Contentful.Core.Extensions;
+using Contentful.Core.Models;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -132,6 +133,129 @@ namespace Contentful.Core.Tests.Extensions
             // Assert
             Assert.Equal("{\"expiresAt\":1620788645000}", convertedString);
 
+        }
+
+        [Fact]
+        public void SystemPropertiesSerializationIgnoresNullValues()
+        {
+            // Arrange
+            var systemProperties = new SystemProperties
+            {
+                Id = "test-asset-id",
+                Type = "Link",
+                LinkType = "Asset"
+                // All other properties are null and should be ignored
+            };
+
+            // Act
+            var convertedString = systemProperties.ConvertObjectToJsonString();
+
+            // Assert
+            Assert.Contains("\"id\":\"test-asset-id\"", convertedString);
+            Assert.Contains("\"type\":\"Link\"", convertedString);
+            Assert.Contains("\"linkType\":\"Asset\"", convertedString);
+            // Should not contain any null values
+            Assert.DoesNotContain("\"archivedAt\":null", convertedString);
+            Assert.DoesNotContain("\"createdAt\":null", convertedString);
+            Assert.DoesNotContain("\"updatedAt\":null", convertedString);
+            Assert.DoesNotContain("\"revision\":null", convertedString);
+        }
+
+        [Fact]
+        public void AssetHyperlinkSerializationWorksCorrectly()
+        {
+            // Arrange
+            var assetHyperlink = new AssetHyperlink
+            {
+                NodeType = "asset-hyperlink",
+                Content = new List<IContent>(),
+                Data = new AssetHyperlinkData
+                {
+                    Target = new Asset
+                    {
+                        SystemProperties = new SystemProperties
+                        {
+                            Id = "test-asset-id",
+                            Type = "Link",
+                            LinkType = "Asset"
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var convertedString = assetHyperlink.ConvertObjectToJsonString();
+
+            // Assert
+            Assert.Contains("\"nodeType\":\"asset-hyperlink\"", convertedString);
+            Assert.Contains("\"id\":\"test-asset-id\"", convertedString);
+            Assert.Contains("\"type\":\"Link\"", convertedString);
+            Assert.Contains("\"linkType\":\"Asset\"", convertedString);
+            // Should not contain any null values
+            Assert.DoesNotContain("\"archivedAt\":null", convertedString);
+            Assert.DoesNotContain("\"createdAt\":null", convertedString);
+            Assert.DoesNotContain("\"updatedAt\":null", convertedString);
+        }
+
+        [Fact]
+        public void UserIssueScenario_AssetHyperlinkWithMinimalSystemProperties()
+        {
+            // This test demonstrates the exact scenario described in the user's issue
+            // where they create an AssetHyperlink with only Id, Type, and LinkType set
+            
+            // Arrange - Simulating the user's code
+            var link = new AssetHyperlink()
+            {
+                NodeType = "embedded-asset-block",
+                Content = new List<IContent>(),
+                Data = new AssetHyperlinkData()
+                {
+                    Target = new Asset()
+                    {
+                        SystemProperties = new SystemProperties()
+                        {
+                            Id = "asset.AssetId",
+                            Type = "Link",
+                            LinkType = "Asset",
+                            // All other properties are null and should be ignored
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var convertedString = link.ConvertObjectToJsonString();
+
+            // Assert - Should only contain the essential fields, no null values
+            Assert.Contains("\"nodeType\":\"embedded-asset-block\"", convertedString);
+            Assert.Contains("\"id\":\"asset.AssetId\"", convertedString);
+            Assert.Contains("\"type\":\"Link\"", convertedString);
+            Assert.Contains("\"linkType\":\"Asset\"", convertedString);
+            
+            // These fields should NOT be present in the JSON
+            Assert.DoesNotContain("\"archivedAt\":null", convertedString);
+            Assert.DoesNotContain("\"createdAt\":null", convertedString);
+            Assert.DoesNotContain("\"updatedAt\":null", convertedString);
+            Assert.DoesNotContain("\"revision\":null", convertedString);
+            Assert.DoesNotContain("\"deletedAt\":null", convertedString);
+            Assert.DoesNotContain("\"locale\":null", convertedString);
+            Assert.DoesNotContain("\"contentType\":null", convertedString);
+            Assert.DoesNotContain("\"publishedCounter\":null", convertedString);
+            Assert.DoesNotContain("\"publishedVersion\":null", convertedString);
+            Assert.DoesNotContain("\"publishedBy\":null", convertedString);
+            Assert.DoesNotContain("\"publishedAt\":null", convertedString);
+            Assert.DoesNotContain("\"publishCounter\":null", convertedString);
+            Assert.DoesNotContain("\"firstPublishedAt\":null", convertedString);
+            Assert.DoesNotContain("\"archivedVersion\":null", convertedString);
+            Assert.DoesNotContain("\"archivedBy\":null", convertedString);
+            Assert.DoesNotContain("\"organization\":null", convertedString);
+            Assert.DoesNotContain("\"usagePeriod\":null", convertedString);
+            Assert.DoesNotContain("\"status\":null", convertedString);
+            Assert.DoesNotContain("\"environment\":null", convertedString);
+            Assert.DoesNotContain("\"space\":null", convertedString);
+            Assert.DoesNotContain("\"createdBy\":null", convertedString);
+            Assert.DoesNotContain("\"updatedBy\":null", convertedString);
+            Assert.DoesNotContain("\"version\":null", convertedString);
         }
 
         [Theory]
